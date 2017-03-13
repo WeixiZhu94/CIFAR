41eaaf9d3e8dcf6f39bc4c050382f36062016e76
Weixi <wxzhu@rice.edu>
2017-03-13 01:31:47 -0500

    sum

--- sum_v15/train.py
+++ sum_v15/train.py
@@ -47,9 +47,9 @@ def main(train_dir, batch_size, num_batches, log_dir, lrn):
     optimizer = tf.train.MomentumOptimizer(lrn, 0.9)
     tf.summary.scalar('learning_rate', lrn)
     total_loss = loss + loss_cat1 + loss_cat2
-   tf.check_numerics(loss_cat1, "loss_1 nan or inf", name=None)
-   tf.check_numerics(loss_cat2, "loss_2 nan orinf", name=None)
-   tf.check_numerics(total_loss, "total_loss nan or inf", name=None)
+    tf.check_numerics(loss_cat1, "loss_1 nan or inf", name=None)
+    tf.check_numerics(loss_cat2, "loss_2 nan orinf", name=None)
+    tf.check_numerics(total_loss, "total_loss nan or inf", name=None)
     train_op = slim.learning.create_train_op(total_loss, optimizer, summarize_gradients=True)
 
     slim.learning.train(train_op, log_dir, save_summaries_secs=20, save_interval_secs=20)
