3d786a01e1b9c5702d94d69fd1d5193acd6b4030
Weixi <wxzhu@rice.edu>
2017-03-13 01:25:51 -0500

    sum

--- sum_v15/model.py
+++ sum_v15/model.py
@@ -50,7 +50,7 @@ def _cat2_logits(logits):
    table1 = tf.constant([1,1,0,0,0,0,0,0,1,1])
    table2 = tf.constant([0,0,1,1,1,1,1,1,0,0])
    A = tf.transpose(tf.stack([table1, table2], axis=0))
-   exp = tf.exp(logits + 0.00001)
+   exp = tf.exp(logits + 0.001)
    return tf.log(tf.matmul(exp, tf.to_float(A)))
 
 def _residual(net, in_filter, out_filter, prefix):
@@ -106,7 +106,7 @@ def network(net, labels):
    loss = tf.losses.sparse_softmax_cross_entropy(labels,logits)
    loss_cat1 = tf.losses.sparse_softmax_cross_entropy(labels_cat1, logits_cat1)
    loss_cat2 = tf.losses.sparse_softmax_cross_entropy(labels_cat2, logits_cat2)
-   Z1 = tf.Print(loss_cat1,[loss_cat1], message="loss1")
-   Z2 = tf.Print(loss_cat2,[loss_cat2], message="loss2")
+   #Z1 = tf.Print(loss_cat1,[loss_cat1], message="loss1")
+   #Z2 = tf.Print(loss_cat2,[loss_cat2], message="loss2")
    return logits, logits_cat1, logits_cat2, loss, loss_cat1, loss_cat2, labels_cat1, labels_cat2
 
