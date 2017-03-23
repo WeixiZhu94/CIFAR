1cb0e644fbf63443edf202c3ff737f3e7726fb0d
Weixi Zhu <Weixi@staff-74-dun20-131.rice.edu>
2017-03-22 19:51:45 -0500

    period 2

--- v12/topdownRes_v12/main.py
+++ v12/topdownRes_v12/main.py
@@ -42,7 +42,7 @@ def train(log_dir, lrn):
 
     optimizer = tf.train.MomentumOptimizer(0.9, lrn)
     tf.summary.scalar('learning_rate', lrn)
-    total_loss = loss_cat2
+    total_loss = loss_cat1
     train_op = slim.learning.create_train_op(total_loss, optimizer, summarize_gradients=True)
 
     slim.learning.train(train_op, log_dir, save_summaries_secs=20, save_interval_secs=20)
